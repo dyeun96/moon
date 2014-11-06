de1872a3df55cd5d939cac7c507b9443efabef38
dyeun96 <dyeun96@naver.com>
2014-11-06 14:44:50 +0900

    c

--- sketch.js
+++ sketch.js
@@ -1,45 +1,69 @@
+var bg;
+var y = 0;
+
 function setup() {
-    createCanvas(500, 500);
+  // The background image must be the same size as the parameters
+  // into the size() method. In this program, the size of the image
+  // is 710x400 pixels.
+  bg = loadImage("space.jpg");
+  createCanvas(500, 500);
 }
 
-
 function draw() {
-   background(255);
-   //태양
+   background(bg);
+  
    fill(255,64,64);
-   stroke(255)
+   noStroke()
     ellipse(250,250,250,250);
-   //달 
+   //태양
    var duration = 4000;
    var timing = (new Date()%duration)/duration;
 
    fill(229,211,136);
-   stroke(255)
+   noStroke()
    ellipse(250 + Math.cos(timing*2*PI)*200,           // x좌표
         250 + Math.sin(timing*2*PI)*200,       // y좌표
         60,                                     // width
         60);                                    // height
-   //타원궤도
+   //타원궤도 행성
    var duration = 5000;
    var timing = (new Date()%duration)/duration;
 
    fill(87,96,255);
-   stroke(255)
+   noStroke()
    ellipse(250 + Math.cos(timing*2*PI)*350,           // x좌표
         250 + Math.sin(timing*2*PI)*200,       // y좌표
         150,                                     // width
         150);                                    // height
-
-   var duration = 6000;
+   //속도 다른 행성
+   var duration = 8000;
    var timing = (new Date()%duration)/duration;
 
    fill(126,22,12);
-   stroke(255)
-   ellipse(250 + Math.cos(timing*2*PI)*200,           // x좌표
+   noStroke()
+   ellipse(250 + Math.cos(timing*2*PI)*250,           // x좌표
         250 + Math.sin(timing*2*PI)*200,       // y좌표
         60,                                     // width
         60);                                    // height
+   //태양를 도는 지구
+   var duration = 6000;
+   var timing = (new Date()%duration)/duration;
 
+   fill(81,158,81);
+   noStroke()
+   ellipse(250 + Math.cos(timing*2*PI)*250,           // x좌표
+        250 + Math.sin(timing*2*PI)*200,       // y좌표
+        60,                                     // width
+        60);                                    // height
+   //지구를 도는 달
+    var duration = 6000;
+   var timing = (new Date()%duration)/duration;
 
+   fill(209,192,70);
+   noStroke()
+   ellipse(250 + Math.cos(timing*2*PI)*250+Math.cos(timing*4*PI)*80,          // x좌표
+        250 + Math.sin(timing*2*PI)*200+Math.sin(timing*4*PI)*80,       // y좌표
+        20,                                     // width
+        20);                                    // height
 }
 
